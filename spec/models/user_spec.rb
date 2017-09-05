55202faffa71502e1eac811b2ee95743ea763a77
Anirudh Vyas <anirudh2403@gmail.com>
2017-09-05 22:08:14 +0530

    Added length validations for name and email in user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,31 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  it { should validate_presence_of(:name)}
-  it { should validate_presence_of(:email)}
+  context "field should be present for validity of object" do
+    it { should validate_presence_of(:name)}
+    it { should validate_presence_of(:email)}
+  end
+
+  describe "length validations" do
+
+    before(:each) do
+      @user = User.new(name: "user", email: "email")
+    end
+
+    context "when length of name is more than 100 chars" do
+      it "should be invalid" do
+        @user.name = 'a'*101
+        expect(@user.valid?).not_to be true
+      end
+    end
+
+    context "when length of name is more than 100 chars" do
+      it "should be invalid" do
+        @user.email = 's'*101
+        expect(@user.valid?).not_to be true
+      end
+    end
+
+  end
 
 end
