1aca772b1a3a5cb479fcb6d72438328f9d618b20
Anirudh Vyas <anirudh2403@gmail.com>
2017-09-08 01:04:18 +0530

    [Anirudh | Divya] Adds JSON response codes to User create API

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,16 @@
 class UserController < ApplicationController
   def create
     puts params
-    @user = User.create!(name: params[:name], email: params[:email])
-    render json: @user, status: :created
+    @user = User.new(name: params[:name], email: params[:email])
+    if (!@user.valid?)
+      render json: {user: @user, message: 'user validation error', status: 400}, status: 400
+    else
+      if (!@user.save)
+        render json: {user: @user, message:'internal server error', status: 500}, status: 500
+      else
+        render json: {user: @user, message:'OK', status: 200}, status: 200
+      end
+    end
   end
 end
 
--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -4,12 +4,17 @@ RSpec.describe UserController, type: :request do
 
   describe "POST /new" do
     let("valid_user") { { name: "anirudh", email: "anirudh.v@go-jek.com" } }
-    before { post '/user', params: valid_user }
+    let("invalid_user") { { name: "asd", email: "ada@gmail.com" } }
+    before { post '/user', params: invalid_user }
 
     context "when the request is valid" do
       it "response should be non-empty" do
         expect(JSON.parse(response.body)).not_to be_empty
       end
+
+
+
     end
+
   end
 end
