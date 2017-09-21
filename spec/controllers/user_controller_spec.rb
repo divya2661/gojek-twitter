95e0d907bffc72e0f27785cb318d8cf88b0b2270
Anirudh Vyas <anirudh2403@gmail.com>
2017-09-21 19:37:53 +0530

    [Anirudh | Divya] User Creation returns 422 on failure

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -4,14 +4,21 @@ RSpec.describe UserController, type: :request do
   describe "POST /new" do
     let("valid_user") { { name: "anirudh", email: "anirudh.v@go-jek.com" } }
     let("invalid_user") { { name: "asd", email: "ada@gmail.com" } }
-    before { post '/user', params: valid_user }
 
     context "when the request is valid" do
+      before { post '/user', params: valid_user }
       it "response should be non-empty" do
         expect(JSON.parse(response.body)).not_to be_empty
       end
       it "response code should be 200" do
         expect(JSON.parse(response.code)).to eq 200
+      end
+    end
+
+    context "when the request is invalid" do
+      before { post '/user', params: invalid_user }
+      it "response code should be 422" do
+        expect(JSON.parse(response.code)).to eq 422
       end
     end
   end
