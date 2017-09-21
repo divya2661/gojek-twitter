dc1a3e357ed713531ba06f2ac7d007bd372665f6
Anirudh Vyas <anirudh2403@gmail.com>
2017-09-21 19:29:49 +0530

    [Anirudh | Divya] Refactor code: change response error codes

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,15 +1,10 @@
 class UserController < ApplicationController
   def create
-    puts params
     @user = User.new(name: params[:name], email: params[:email])
-    if (!@user.valid?)
-      render json: {user: @user, message: 'user validation error', status: 400}, status: 400
+    if @user.save
+      render json: {user: @user, message:'OK', status: 200}, status: 200
     else
-      if (!@user.save)
-        render json: {user: @user, message:'internal server error', status: 500}, status: 500
-      else
-        render json: {user: @user, message:'OK', status: 200}, status: 200
-      end
+      render json: {user: @user, message: @user.errors.full_messages, status: 422}, status: 422
     end
   end
 end
--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,20 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe UserController, type: :request do
-
   describe "POST /new" do
     let("valid_user") { { name: "anirudh", email: "anirudh.v@go-jek.com" } }
     let("invalid_user") { { name: "asd", email: "ada@gmail.com" } }
-    before { post '/user', params: invalid_user }
+    before { post '/user', params: valid_user }
 
     context "when the request is valid" do
       it "response should be non-empty" do
         expect(JSON.parse(response.body)).not_to be_empty
       end
-
-
-
     end
-
   end
 end
