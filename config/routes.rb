18b1a02fa6cf140510e4b7ab474b6c6bb42dca5b
divya.n <divya.n@go-jek.com>
2017-10-30 23:53:16 +0530

    Adds environment port in puma configuration for heroku and removes helloworld route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'hello_world', to: 'hello_world#index'
-  get 'registration', to: 'registration#index'
+   get 'registration', to: 'registration#index'
   resources :user
 end
 
