b199b2b1908ec0b7492891288103dc8034091340
divya.n <divya.n@go-jek.com>
2017-11-07 17:12:42 +0700

    Removes unused code and files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'tweet/new'
-
-  get 'registration', to: 'registration#index'
+    get 'registration', to: 'registration#index'
   get 'tweet', to: 'tweet#index'
   resources :user
   resources :tweet
